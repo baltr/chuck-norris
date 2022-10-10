1eddeb5bfa3d3fcb571bd28ee390d370056a8508
Baltr <markorukavina4@gmail.com>
2022-10-10 20:09:16 +0200

    Added functionality for showing and deleting cached favourite jokes

--- src/views/Favourites.vue
+++ src/views/Favourites.vue
@@ -4,5 +4,19 @@
         <router-link to="/Favourites"><button>Favourites</button></router-link>
         <router-link to="/Categories"><button>Categories</button></router-link>
         <router-link to="/Search"><button>Search</button></router-link>
+        <div v-for="joke in $store.getters.getFavouriteJokes" :key="joke">
+            <Joke :joke="joke"/>
+            <button @click="$store.commit('removeFavouriteJoke', joke)">Remove</button>
+        </div> 
     </div>
 </template>
+
+<script>
+import Joke from "@/components/Joke.vue"
+
+export default {
+    components:{
+        Joke
+    }
+}
+</script>
